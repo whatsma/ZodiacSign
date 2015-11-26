dc097a89a60652a21f024dfcd790ba4e1d85cbfe
John <john@lentil.local>
2015-11-26 19:45:44 +0000

    added test

---
+++ tests/CheckEverySignTest.php
@@ -0,0 +1,24 @@
+<?php
+use Whatsma\ZodiacSign\Calculator;
+ 
+class CheckEverySignTest extends PHPUnit_Framework_TestCase {
+ 
+    public function testInvalidMonth()
+        {
+        $calculator = new Calculator;
+
+        $this->assertTrue($calculator->calculate(1, 1) == "capricorn");
+        $this->assertTrue($calculator->calculate(1, 2) == "aquarius");
+        $this->assertTrue($calculator->calculate(1, 3) == "pisces");
+        $this->assertTrue($calculator->calculate(1, 4) == "aries");
+        $this->assertTrue($calculator->calculate(1, 5) == "taurus");
+        $this->assertTrue($calculator->calculate(1, 6) == "gemini");
+        $this->assertTrue($calculator->calculate(1, 7) == "cancer");
+        $this->assertTrue($calculator->calculate(1, 8) == "leo");
+        $this->assertTrue($calculator->calculate(1, 9) == "virgo");
+        $this->assertTrue($calculator->calculate(1, 10) == "libra");
+        $this->assertTrue($calculator->calculate(1, 11) == "scorpio");
+        $this->assertTrue($calculator->calculate(1, 12) == "sagittarius");
+        }
+
+}
