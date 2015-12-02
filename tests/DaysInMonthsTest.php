bef1b727325cd37abbb430e53e938291a45c7c86
zodiac <whatsma@whatzodiacsignami.com>
2015-12-02 20:33:35 +0000

    updated tests to match code changes

--- tests/DaysInMonthsTest.php
+++ tests/DaysInMonthsTest.php
@@ -7,18 +7,18 @@ class DaysInMonthsTest extends PHPUnit_Framework_TestCase {
         {
         $calculator = new Calculator;
 
-        $this->assertTrue($calculator->days_in_month(1) == 31);
-        $this->assertTrue($calculator->days_in_month(2) == 29);
-        $this->assertTrue($calculator->days_in_month(3) == 31);
-        $this->assertTrue($calculator->days_in_month(4) == 30);
-        $this->assertTrue($calculator->days_in_month(5) == 31);
-        $this->assertTrue($calculator->days_in_month(6) == 30);
-        $this->assertTrue($calculator->days_in_month(7) == 31);
-        $this->assertTrue($calculator->days_in_month(8) == 31);
-        $this->assertTrue($calculator->days_in_month(9) == 30);
-        $this->assertTrue($calculator->days_in_month(10) == 31);
-        $this->assertTrue($calculator->days_in_month(11) == 30);
-        $this->assertTrue($calculator->days_in_month(12) == 31);
+        $this->assertTrue($calculator->daysInMonth(1) == 31);
+        $this->assertTrue($calculator->daysInMonth(2) == 29);
+        $this->assertTrue($calculator->daysInMonth(3) == 31);
+        $this->assertTrue($calculator->daysInMonth(4) == 30);
+        $this->assertTrue($calculator->daysInMonth(5) == 31);
+        $this->assertTrue($calculator->daysInMonth(6) == 30);
+        $this->assertTrue($calculator->daysInMonth(7) == 31);
+        $this->assertTrue($calculator->daysInMonth(8) == 31);
+        $this->assertTrue($calculator->daysInMonth(9) == 30);
+        $this->assertTrue($calculator->daysInMonth(10) == 31);
+        $this->assertTrue($calculator->daysInMonth(11) == 30);
+        $this->assertTrue($calculator->daysInMonth(12) == 31);
         }
 
 }
