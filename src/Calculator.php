2d83da9b7bac4d23ce83689d360f5bf0dd4fae30
zodiac <whatsma@whatzodiacsignami.com>
2015-12-10 19:25:31 +0000

    simplified calculator

--- src/Calculator.php
+++ src/Calculator.php
@@ -5,12 +5,6 @@ class Calculator
 {
     public function calculate($day, $month)
     {
-        // validate month
-        $month = intval($month);
-        if (($month < 1) || ($month > 12)) {
-            throw new InvalidMonthException();
-        }
-    
         // validate day
         $day = intval($day);
         if (($day < 1) || ($day > $this->daysInMonth($month))) {
@@ -19,30 +13,106 @@ class Calculator
     
     
         // calculate zodiac sign
-        if (($month==1 && $day>=21) || ($month==2 && $day<=19)) {
-             return "aquarius";
-        } elseif (($month==2 && $day>=20 ) || ($month==3 && $day<=20)) {
-             return "pisces";
-        } elseif (($month==3 && $day>=21) || ($month==4 && $day<=20)) {
-             return "aries";
-        } elseif (($month==4 && $day>=21) || ($month==5 && $day<=21)) {
-             return "taurus";
-        } elseif (($month==5 && $day>=22) || ($month==6 && $day<=21)) {
-             return "gemini";
-        } elseif (($month==6 && $day>=22) || ($month==7 && $day<=22)) {
-             return "cancer";
-        } elseif (($month==7 && $day>=23) || ($month==8 && $day<=22)) {
-             return "leo";
-        } elseif (($month==8 && $day>=23) || ($month==9 && $day<=23)) {
-             return "virgo";
-        } elseif (($month==9 && $day>=24) || ($month==10 && $day<=23)) {
-             return "libra";
-        } elseif (($month==10 && $day>=24) || ($month==11 && $day<=22)) {
-             return "scorpio";
-        } elseif (($month==11 && $day>=23) || ($month==12 && $day<=21)) {
-             return "sagittarius";
-        } elseif (($month==12 && $day>=22) || ($month==1 && $day<=20)) {
-             return "capricorn";
+        switch ($month) 
+        {
+            case 1: // january
+                if ($day <= 20) {
+                    return "capricorn";
+                } else {
+                    return "aquarius";
+                }
+                break;
+
+            case 2: // february
+                if ($day <= 19) {
+                    return "aquarius";
+                } else {
+                    return "pisces";
+                }
+                break;
+
+            case 3: // march
+                if ($day <= 20) {
+                    return "pisces";
+                } else {
+                    return "aries";
+                }
+                break;
+
+            case 4: // april
+                if ($day <= 20) {
+                    return "aries";
+                } else {
+                    return "taurus";
+                }
+                break;
+
+            case 5: // may
+                if ($day <= 21) {
+                    return "taurus";
+                } else {
+                    return "gemini";
+                }
+                break;
+
+            case 6: // june
+                if ($day <= 21) {
+                    return "gemini";
+                } else {
+                    return "cancer";
+                }
+                break;
+
+            case 7: // july
+                if ($day <= 22) {
+                    return "cancer";
+                } else {
+                    return "leo";
+                }
+                break;
+
+            case 8: // august
+                if ($day <= 22) {
+                    return "leo";
+                } else {
+                    return "virgo";
+                }
+                break;
+
+            case 9: // september
+                if ($day <= 23) {
+                    return "virgo";
+                } else {
+                    return "libra";
+                }
+                break;
+
+            case 10: // october
+                if ($day <= 23) {
+                    return "libra";
+                } else {
+                    return "scorpio";
+                }
+                break;
+
+            case 11: // november
+                if ($day <= 22) {
+                    return "scorpio";
+                } else {
+                    return "sagittarius";
+                }
+                break;
+
+            case 12: // december
+                if ($day <= 21) {
+                    return "sagittarius";
+                } else {
+                    return "capricorn";
+                }
+                break;
+
+            default:
+                throw new InvalidMonthException();
         }
     }
 
